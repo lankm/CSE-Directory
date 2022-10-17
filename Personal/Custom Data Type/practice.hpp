0655b5ca4c4696208704b1c0425d8c25dd8ad45e
Landon Moon <lankm@live.com>
2022-10-17 15:05:13 -0500

    started cpp version of bignum

---
+++ Personal/Custom Data Type/practice.hpp
@@ -0,0 +1,111 @@
+/* This is a simple implementation of a 32.32 bit fixed point number.
+ * The goal of this project of mine is to be able to make a low level
+ * implementation of bigDecimal that runs as fast as possible.
+ * 
+ * Because decimal values exist, signed integers should not be used.
+ * They don't inform decimal values about their negativity.
+ * 
+ * Some methods might be implemented with assembally.
+ *
+ * Author: Landon Moon
+ * In progress
+ * 
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+using namespace std;
+
+class Number
+{
+    private:
+        uint32_t f;
+        uint32_t i;
+        uint8_t  n : 1;
+    public:
+        Number()
+        {
+            f=0;
+            i=0;
+            n=0;
+        }
+        Number(const char *str)
+        {
+            // retrieving decimal bits
+            char* point = strchr(str, '.');
+            double dec = atof(point);
+            f = (uint32_t)(dec*(1<<16)*(1<<16));    // err: small rounding mistake
+
+            // if negative
+            if(str[0] == '-')
+            {
+                // retrieving integer bits. skip '-'
+                i = atoll(++str);
+
+                // negative handling
+                n = 1;
+            }
+            else
+            {
+                // retrieving integer bits
+                i = atoll(str);
+            }
+
+            printf("adr: %p i: %p\n", &f, &i); // print address values
+            printf("num: %d %08lx.%08x\n", n, i, f); // print bit values
+        }
+        char* toString(char *str)
+        {
+            // if negative
+            char c = '+';
+            if(n)
+            {
+                c = '-';
+            }
+
+            // retrieving decimal value
+            double dec = ((double)(f))/(1<<16)/(1<<16);
+    
+            // string handling
+            char strBuff[11];
+            sprintf(strBuff, "%.9lf", dec);
+            sprintf(str, "%c%lld%s\n", c, i, strBuff+1);
+
+            return str;
+        }
+        Number operator +(const Number num)
+        {
+            Number temp;
+            
+            //add
+
+            return temp;
+        }
+        Number operator -(const Number num)
+        {
+            Number temp;
+            
+            //sub
+
+            return temp;
+        }
+        Number operator *(const Number num)
+        {
+            Number temp;
+            
+            //mul
+
+            return temp;
+        }
+        Number operator /(const Number num)
+        {
+            Number temp;
+            
+            //div
+
+            return temp;
+        }
+};
